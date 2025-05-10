da467892cdb0b57fb5b6dfed94673328b6104205
svpontes <svpontes2015@gmail.com>
2025-05-10 14:39:33 +0100

    add line: const cartItems = getLocalStorage("so-cart") || [];//assure the array will be empty if getLocalStorage returned null

--- src/js/product.js
+++ src/js/product.js
@@ -4,11 +4,11 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product)
+  const cartItems = getLocalStorage("so-cart") || [];//assure the array will be empty if getLocalStorage returned null
+  cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
