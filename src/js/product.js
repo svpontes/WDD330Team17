13b9b01f781129a5ace262fc6b6c56f6d710c893
svpontes <svpontes2015@gmail.com>
2025-05-10 14:58:02 +0100

    fix error from cart operation

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,7 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];//assure the array will be empty if getLocalStorage returned null
+  const cartItems = getLocalStorage("so-cart") || []; //assure the array will be empty if getLocalStorage returned null
   cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
 }
