851195a994f7b4788a5ef57f7401bfc002fc3331
svpontes <svpontes2015@gmail.com>
2025-05-16 01:37:38 +0100

    place code in the right file product.js and productDetails

--- src/js/product.js
+++ src/js/product.js
@@ -1,21 +1,27 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+// Import the getParam function from the utils.mjs module
+// This function is used to get query parameters from the URL (e.g., ?product=810NV)
+import { getParam } from "./utils.mjs";
+
+// Import the ProductData class from the ProductData.mjs module
+// This class is responsible for loading product data from a JSON file
 import ProductData from "./ProductData.mjs";
 
+// Import the ProductDetails class from the ProductDetails.mjs module
+// This class handles finding a specific product and rendering its details on the page
+import ProductDetails from "./ProductData.mjs";
+
+// Create a new instance of ProductData for the "tents" category
+// This means it will load data from the "tents" JSON file
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || []; //assure the array will be empty if getLocalStorage returned null
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
+// Get the value of the "product" query parameter from the URL
+// Example: if the URL is product.html?product=810NV → returns "810NV"
+const productID = getParam("product");
 
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// Create an instance of the ProductDetails class, passing in the product ID and data source
+// This object now has everything it needs to load and display the product info
+const product = new ProductDetails(productID, dataSource);
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// Call the init() method from the ProductDetails class
+// This method will fetch the product data and render it into the HTML
+product.init();
