68da35d4f8b6f71108b5db9c1bef24cc2ecef3cd
svpontes <svpontes2015@gmail.com>
2025-05-21 19:34:20 +0100

    changes on main added load to footer and header

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -29,3 +29,27 @@ export function renderListWithTemplate(templateFn, parentElement, list, position
   parentElement.insertAdjacentHTML(position, htmlString.join(""));
 }
 
+export function renderWithTemplate(template, parentElement, data = {}, callback = null) {
+    parentElement.innerHTML = template;
+    if(callback) {
+      callback(data);
+    }
+}
+  
+export async function loadTemplate(path) {
+  
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+
+  const headerElement = document.querySelector("#main-header");
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}
